9f67cb170a17ff87292f856000d561f3dcabcba6
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-05-25 18:18:53 -0300

    cria home

--- mulheres-no-game/src/components/Header/Header.jsx
+++ mulheres-no-game/src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
 
 export const Header = () => {
   return (
-    <div>
-        <p>GameHer</p>
-        <Link to="/login"><button>Login</button></Link>
-        <Link to="/cadastro"><button>Cadastre-se</button></Link>
+    <div className={styles.header}>
+        <p>Logo</p>
+        <div className={styles.wrapBtn}>
+            <Link to="/login"><button className={styles.btnHome}>Login</button></Link>
+            <Link to="/cadastro"><button className={styles.btnHome}>Cadastre-se</button></Link>
+        </div>
     </div>
   )
 }
--- mulheres-no-game/src/pages/Home/Home.jsx
+++ mulheres-no-game/src/pages/Home/Home.jsx
@@ -1,36 +1,45 @@
 import { Link } from 'react-router-dom';
-import { Header } from '../../components/Header/Header'
+import { Header } from '../../components/Header/Header';
+import styles from './Home.module.css';
 
 export const Home = () => {
   return (
     <div>
       <Header/>
-      <section>
-        <h1>Empoderando Mulheres no Mundo dos Jogos</h1>
-        <p>Bem-vinda à nossa plataforma, onde empoderamos mulheres a iniciarem sua jornada no emocionante universo do desenvolvimento de jogos.</p>
+      <section className={styles.banner}>
+        <div className={styles.contentBanner}>
+          <h1>Empoderando Mulheres no Mundo dos Jogos</h1>
+          <p>Bem-vinda à nossa plataforma, onde empoderamos mulheres a iniciarem sua jornada no emocionante universo do desenvolvimento de jogos.</p>
+        </div>
       </section>
-      <section>
-        <h2>Nossa motivação</h2>
-        <p>A indústria dos jogos é um campo dinâmico e em rápido crescimento, repleto de possibilidades infinitas. No entanto, historicamente, tem sido dominada por homens, e as mulheres frequentemente enfrentam desafios únicos ao ingressarem nesse setor.</p>
-        <p>Nossa missão é mudar essa narrativa, proporcionando às mulheres as ferramentas, o conhecimento e o suporte necessários para alcançarem o sucesso.</p>
-        <h2>O que é a plataforma?</h2>
-        <p>Nossos cursos introdutórios são projetados para quebrar barreiras e criar um ambiente acolhedor e inclusivo para mulheres interessadas no desenvolvimento de jogos.</p>
-        <p>Seja você uma iniciante completa ou alguém com alguma experiência, nossos cursos abrangem uma ampla variedade de tópicos, incluindo design de jogos, programação, narrativa e design gráfico. Você aprenderá com profissionais experientes e se conectará com uma comunidade de mulheres que compartilham sua paixão por jogos.</p>
-      </section>
-      <section>
-        <h3>Vamos começar uma nova carreira!</h3>
-        <p>Clique para logar e começar os cursos e dar um start na sua carreira!</p>
-        <Link to="/cadastro"><button>Cadastre-se</button></Link>
-      </section>
-      <section>
-        <h4>Ao se inscrever em nossos cursos, você ganhará:</h4>
-        <ul>
-          <li>Conhecimento Abrangente: Aprenda os fundamentos do desenvolvimento de jogos do zero, incluindo as tecnologias mais recentes e os padrões da indústria.</li>
-          <li>Habilidades Práticas: Envolva-se em projetos práticos que permitem aplicar o que você aprendeu e construir seu portfólio.</li>
-          <li>Oportunidades de Networking: Conecte-se com outras mulheres na indústria, forme relacionamentos valiosos e encontre oportunidades de mentoria.</li>
-          <li>Orientação de Carreira: Receba conselhos e suporte sobre como navegar na indústria dos jogos, desde a busca por empregos até o avanço na carreira.</li>
-        </ul>
-      </section>
+      <div className={styles.wrapHome}>
+        <section>
+          <h2>Nossa motivação</h2>
+          <p>A indústria dos jogos é um campo dinâmico e em rápido crescimento, repleto de possibilidades infinitas. No entanto, historicamente, tem sido dominada por homens, e as mulheres frequentemente enfrentam desafios únicos ao ingressarem nesse setor.</p>
+          <p className={styles.textMotivacao}>Nossa missão é mudar essa narrativa, proporcionando às mulheres as ferramentas, o conhecimento e o suporte necessários para alcançarem o sucesso.</p>
+          <h2>O que é a plataforma?</h2>
+          <p className={styles.textPlataforma}>Nossos cursos introdutórios são projetados para quebrar barreiras e criar um ambiente acolhedor e inclusivo para mulheres interessadas no desenvolvimento de jogos.</p>
+          <p>Seja você uma iniciante completa ou alguém com alguma experiência, nossos cursos abrangem uma ampla variedade de tópicos, incluindo design de jogos, programação, narrativa e design gráfico. Você aprenderá com profissionais experientes e se conectará com uma comunidade de mulheres que compartilham sua paixão por jogos.</p>
+        </section>
+      </div>
+        <section className={styles.ctaCadastro}>
+          <h3>Vamos começar uma <br />nova carreira!</h3>
+          <div className={styles.wrapCta}>
+            <p>Clique para logar e começar os cursos e dar um start na sua carreira!</p>
+            <Link to="/cadastro"><button className={styles.btnHome}>Cadastre-se</button></Link>
+          </div>
+        </section>
+      <div className={styles.wrapHome}>
+        <section className={styles.container}>
+          <h4>Ao se inscrever em nossos cursos, você ganhará:</h4>
+          <ul>
+            <li><strong>Conhecimento Abrangente:</strong> Aprenda os fundamentos do desenvolvimento de jogos do zero, incluindo as tecnologias mais recentes e os padrões da indústria.</li>
+            <li><strong>Habilidades Práticas:</strong> Envolva-se em projetos práticos que permitem aplicar o que você aprendeu e construir seu portfólio.</li>
+            <li><strong>Oportunidades de Networking:</strong> Conecte-se com outras mulheres na indústria, forme relacionamentos valiosos e encontre oportunidades de mentoria.</li>
+            <li><strong>Orientação de Carreira:</strong> Receba conselhos e suporte sobre como navegar na indústria dos jogos, desde a busca por empregos até o avanço na carreira.</li>
+          </ul>
+        </section>
+      </div>
     </div>
   )
 }

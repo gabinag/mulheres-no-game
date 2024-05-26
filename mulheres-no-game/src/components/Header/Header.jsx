ec5458dccdc6a500af40327ccd7d1bf6887b5817
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-05-26 17:08:17 -0300

    Update logo

--- mulheres-no-game/src/components/Header/Header.jsx
+++ mulheres-no-game/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+import logo from '../../assets/logonome.png'; 
 
 export const Header = () => {
   return (
     <div className={styles.header}>
-        <p>Academy of Goddesses</p>
+        <img src={logo} alt="" />
         <div className={styles.wrapBtn}>
             <Link to="/login"><button className={styles.btnHome}>Login</button></Link>
             <Link to="/cadastro"><button className={styles.btnHome}>Cadastre-se</button></Link>

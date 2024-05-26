6844e80652111d94f51f227cf33da21ab0089087
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-05-26 16:43:29 -0300

    Update logo

--- mulheres-no-game/src/components/Header/Header.jsx
+++ mulheres-no-game/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 export const Header = () => {
   return (
     <div className={styles.header}>
-        <p>Logo</p>
+        <p>Academy of Goddesses</p>
         <div className={styles.wrapBtn}>
             <Link to="/login"><button className={styles.btnHome}>Login</button></Link>
             <Link to="/cadastro"><button className={styles.btnHome}>Cadastre-se</button></Link>

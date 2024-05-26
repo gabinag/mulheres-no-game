3a2594609e58af3a73d4b185b84d8ecdb601da6f
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-05-25 22:04:34 -0300

    Resolver conflitos

--- mulheres-no-game/src/App.jsx
+++ mulheres-no-game/src/App.jsx
@@ -5,11 +5,8 @@ import './App.css';
 import { Home } from './pages/Home/Home';
 import { Login } from './pages/Login/Login';
 import { Cadastro } from './pages/Cadastro/Cadastro';
-<<<<<<< HEAD
 import { Ilustracao } from './pages/Ilustracao/Ilustracao';
-=======
 import { Comecar } from './pages/Comecar/Comecar';
->>>>>>> 4a08d15d8202e40adf9c02865391814ca2940d09
 
 function App() {
 
@@ -19,11 +16,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
-<<<<<<< HEAD
         <Route path="/comecar/ilustracao" element={<Ilustracao />} />
-=======
         <Route path='/comecar' element={<Comecar />} />
->>>>>>> 4a08d15d8202e40adf9c02865391814ca2940d09
       </Routes>
     </BrowserRouter>
   )

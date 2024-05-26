e2808a13bf934a811977b058f213c1439100965c
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-05-26 20:13:48 -0300

    add navegacao da home pelo logo

--- mulheres-no-game/src/components/HeaderLogado/HeaderLogado.jsx
+++ mulheres-no-game/src/components/HeaderLogado/HeaderLogado.jsx
@@ -5,7 +5,7 @@ import logo from '../../assets/logonome.png';
 export const HeaderLogado = () => {
   return (
     <div className={styles.header}>
-        <img src={logo} alt="Logotipo" />
+        <Link to="/"><img src={logo} alt="Logotipo" /></Link>
         <div className={styles.wrapBtn}>
             <Link to="/comecar" className={styles.guia}><p>Guia de Carreira</p></Link>
             <Link to="/"><button className={styles.btnSair}>Sair</button></Link>

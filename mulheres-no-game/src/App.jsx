025c667126604dd4fe8f07882e678383a9a929c4
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-05-25 21:59:27 -0300

    Cria area especifica

--- mulheres-no-game/src/App.jsx
+++ mulheres-no-game/src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
+import './App.css';
 
 //pages
 import { Home } from './pages/Home/Home';
 import { Login } from './pages/Login/Login';
 import { Cadastro } from './pages/Cadastro/Cadastro';
+import { Ilustracao } from './pages/Ilustracao/Ilustracao';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/" element={<Ilustracao />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ mulheres-no-game/src/pages/Ilustracao/Ilustracao.jsx
@@ -0,0 +1,15 @@
+import { AreaEspecifica } from "../../components/AreaEspecifica/AreaEspecifica"
+
+export const Ilustracao = () => {
+  return (
+    <div>
+        <AreaEspecifica 
+        title="Ilustração" 
+        paragraph="O ilustrador é o artista responsável por criar ilustrações finalizadas que serão usadas para promover o jogo no mercado, como pôsteres, arte de capa, wallpapers, banners e até mesmo o logo do jogo." 
+        item1="Noções de desenho" 
+        item2="Noções de cores" 
+        item3="Pintura Digital" 
+        item4="Ferramentas de ilustração"/>
+    </div>
+  )
+}
